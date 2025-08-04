4b52cfa076c4471ba63ec819c4835967430ceaae
alejandro <avenegasc@unap.cl>
2025-08-04 17:37:46 -0400

    lunes

--- src/app/ricky-morty/pages/catalogo/catalogo.component.ts
+++ src/app/ricky-morty/pages/catalogo/catalogo.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import CharactersComponent from './componentes/character-card/character-card.component';
 import { characterService } from '../../services/rickmorty.service';
+import  NavBarComponent  from '../../componentes/navbar/navbar';
 
 @Component({
   selector: 'app-catalogo',
-  imports: [CharactersComponent],
+  imports: [CharactersComponent, NavBarComponent],
   templateUrl: './catalogo.component.html',
 
 })
